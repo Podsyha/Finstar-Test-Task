12b6838491868edd5ddcf3398141b1ea0d2de096
Александр Подсухин <podsyha@gmail.com>
2022-10-26 20:26:17 +0500

    Почистил мидлу

--- Common/Middlewares/ExceptionMiddleware.cs
+++ Common/Middlewares/ExceptionMiddleware.cs
@@ -9,19 +9,16 @@ namespace FINSTAR_Test_Task.Common.Middlewares;
 /// </summary>
 public sealed class ExceptionMiddleware
 {
-    public ExceptionMiddleware(RequestDelegate nextRequestDelegate, ILogger<ExceptionMiddleware> logger)
+    public ExceptionMiddleware(RequestDelegate nextRequestDelegate)
     {
         _nextRequestDelegate = nextRequestDelegate;
-        _logger = logger;
     }
 
     private readonly RequestDelegate _nextRequestDelegate;
-    private readonly ILogger<ExceptionMiddleware> _logger;
 
 
     public async Task InvokeAsync(HttpContext context)
     {
-
         try
         {
             await _nextRequestDelegate(context);
@@ -40,25 +37,8 @@ public sealed class ExceptionMiddleware
                 _ => (int)HttpStatusCode.BadRequest
             };
 
-            _logger.LogError(error.Message);
-
             string result = JsonSerializer.Serialize(error.Message);
             await response.WriteAsync(result);
         }
-    }
-    
-    private static string FormatHeaders(IHeaderDictionary headers) => string.Join(", ", headers.Select(kvp => $"{{{kvp.Key}: {string.Join(", ", kvp.Value)}}}"));
-
-    private static async Task<string> ReadBodyFromRequest(HttpRequest request)
-    {
-        // Ensure the request's body can be read multiple times (for the next middlewares in the pipeline).
-        request.EnableBuffering();
-
-        using var streamReader = new StreamReader(request.Body, leaveOpen: true);
-        var requestBody = await streamReader.ReadToEndAsync();
-
-        // Reset the request's body stream position for next middleware in the pipeline.
-        request.Body.Position = 0;
-        return requestBody;
     }
 }
